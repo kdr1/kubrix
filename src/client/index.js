e6f5909021f1b4fff24f13527f8efa5f4050902c
Kyle Richardson <kyle.richardson1981@gmail.com>
2017-07-04 23:32:08 -0400

    initial sectioning

--- src/client/index.js
+++ src/client/index.js
@@ -12,7 +12,55 @@ class Kubrix extends PureComponent {
 
 		this.three = { };
 
-		this.kube = { };
+		this.kube = {
+			whole: null,
+			reference: {
+				c1: null,
+				c2: null,
+				c3: null,
+				c4: null,
+				c5: null,
+				c6: null,
+				c7: null,
+				c8: null,
+				c9: null,
+				c10: null,
+				c11: null,
+				c12: null,
+				c13: null,
+				c14: null,
+				c15: null,
+				c16: null,
+				c17: null,
+				c18: null,
+				c19: null,
+				c20: null,
+				c21: null,
+				c22: null,
+				c23: null,
+				c24: null,
+				c25: null,
+				c26: null,
+				c27: null
+			},
+			sections: {
+				x: {
+					left: [ ],
+					middle: [ ],
+					right: [ ]
+				},
+				y: {
+					front: [ ],
+					middle: [ ],
+					back: [ ]
+				},
+				z: {
+					top: [ ],
+					middle: [ ],
+					bottom: [ ]
+				}
+			}
+		};
 		this.previousMouseLocation = {
 			x: 0,
 			y: 0
@@ -54,9 +102,9 @@ class Kubrix extends PureComponent {
 
 		// create and add 3js PerspectiveCamera to scene
 		this.three.Camera = new THREE.PerspectiveCamera( 40, WIDTH / HEIGHT, 1, 100 );
-		this.three.Camera.position.x = 0;
+		this.three.Camera.position.x = 12;
 		this.three.Camera.position.y = 8;
-		this.three.Camera.position.z = 16;
+		this.three.Camera.position.z = 12;
 		this.three.Scene.add( this.three.Camera );
 
 		// add a little illumination
@@ -94,6 +142,85 @@ class Kubrix extends PureComponent {
 					child.position.y = 20;
 					child.position.z = 15;
 				}
+				switch ( child.name ) {
+					case "c1":
+						this.kube.reference.c1 = child;
+						this.kube.sections.x.left.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.top.push( child.name );
+						break;
+					case "c2":
+						this.kube.reference.c2 = child;
+						this.kube.sections.x.middle.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.top.push( child.name );
+						break;
+					case "c3":
+						this.kube.reference.c3 = child;
+						this.kube.sections.x.right.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.top.push( child.name );
+						break;
+					case "c4":
+						this.kube.reference.c4 = child;
+						this.kube.sections.x.left.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.middle.push( child.name );
+						break;
+					case "c5":
+						this.kube.reference.c5 = child;
+						this.kube.sections.x.middle.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.middle.push( child.name );
+						break;
+					case "c6":
+						this.kube.reference.c6 = child;
+						this.kube.sections.x.right.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.middle.push( child.name );
+						break;
+					case "c7":
+						this.kube.reference.c7 = child;
+						this.kube.sections.x.left.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.bottom.push( child.name );
+						break;
+					case "c8":
+						this.kube.reference.c8 = child;
+						this.kube.sections.x.middle.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.bottom.push( child.name );
+						break;
+					case "c9":
+						this.kube.reference.c9 = child;
+						this.kube.sections.x.right.push( child.name );
+						this.kube.sections.y.front.push( child.name );
+						this.kube.sections.z.bottom.push( child.name );
+						break;
+					case "c10":
+					case "c11":
+					case "c12":
+					case "c13":
+					case "c14":
+					case "c15":
+					case "c16":
+					case "c17":
+					case "c18":
+					case "c19":
+					case "c20":
+					case "c21":
+					case "c22":
+					case "c23":
+					case "c24":
+					case "c25":
+					case "c26":
+					case "c27":
+						break;
+				}
+			} );
+
+			this.kube.sections.y.front.forEach( ( faceName ) => {
+				this.kube.reference[ faceName ].rotation.y -= Math.PI / 2;
 			} );
 
 			// add object to scene
